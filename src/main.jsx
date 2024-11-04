0d2f79308b63b405590bc4ab97e545d8e16d39e1
Orlando Verdin <git@orli.land>
2024-11-04 14:20:35 -0700

    install and config fonts (inter, lora, inconsolata)

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+// Supports weights 100-900
+import '@fontsource-variable/inter';
+// Supports weights 400-700
+import '@fontsource-variable/lora';
+// Supports weights 200-900
+import '@fontsource-variable/inconsolata';
+
 import "./output.css";
 import App from "./App.jsx";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
+    fontFamily:{
+      sans: ['Inter Variable', 'sans-serif'],
+      serif: ['Lora Variable', "serif"],
+      mono: ['Inconsolata Variable', "monospace"] 
+    },
     extend: {},
   },
   plugins: [],
