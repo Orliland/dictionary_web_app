0b4a354e1ed9d69262c1801068db28ee60a92fee
Orlando Verdin <git@orli.land>
2024-11-04 14:31:32 -0700

    add colors custom variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
       serif: ['Lora Variable', "serif"],
       mono: ['Inconsolata Variable', "monospace"] 
     },
+    colors: {
+      black: {
+        100: "#3a3a3a",
+        200: "#2d2d2d",
+        300: '#1f1f1f',
+        400: '#050505'
+      },
+      white: {
+        100: '#ffffff',
+        200: '#f4f4f4',
+        300: '#e9e9e9',
+        400: '#757575'
+      },
+      red: '#FF5252',
+      purple: '#A445ED'
+    },
     extend: {},
   },
   plugins: [],
